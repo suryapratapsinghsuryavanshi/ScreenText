5fc939023dde70bab640c3ca5cd49c4531cd23bb
Suryapratap Singh Suryavanshi <suryprtaps@gmail.com>
2024-05-19 09:23:13 +0530

    fix: made os[first class member] and try to minimize error regarding os competibility

--- ScreenText.py
+++ ScreenText.py
@@ -1,10 +1,30 @@
+import os
 import PyInstaller.__main__
+
+if os.name == 'nt':  # Windows
+    binaries = [
+        'C:/Program Files/Tesseract-OCR/tesseract.exe;Tesseract-OCR',
+        'C:/Program Files/Tesseract-OCR/tessdata;Tesseract-OCR/tessdata'
+    ]
+elif os.name == 'posix':
+    if os.uname().sysname == 'Darwin':  # macOS
+        binaries = [
+            '/usr/local/bin/tesseract;tesseract',
+            '/usr/local/share/tessdata;tessdata'
+        ]
+    else:  # Linux
+        binaries = [
+            '/usr/bin/tesseract;tesseract',
+            '/usr/share/tesseract-ocr/4.00/tessdata;tessdata'
+        ]
+else:
+    raise RuntimeError(f"Unsupported OS: {os.name}")
 
 PyInstaller.__main__.run([
     'main.py',
     '--name=ScreenText',
     '--onefile',
     '--windowed',
-    '--add-binary', 'C:/Program Files/Tesseract-OCR/tesseract.exe;Tesseract-OCR',
-    '--add-binary', 'C:/Program Files/Tesseract-OCR/tessdata;Tesseract-OCR/tessdata'
+    '--add-binary', binaries[0],
+    '--add-binary', binaries[1]
 ])
